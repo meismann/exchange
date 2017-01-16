9cd7ea63e1e662f600e0f412a2beaab2fc308624
Martin Eismann <softwerk@eismann.cc>
2017-01-16 10:32:17 +0100

    Add Exchange::Money#amount

--- lib/exchange/money.rb
+++ lib/exchange/money.rb
@@ -1,7 +1,17 @@
 module Exchange
   class Money
 
+    attr_reader :amount
+
+    class << self
+
+      def conversion_rates(base_currency, conversion_rates)
+      end
+
+    end
+
     def initialize(amount, currency)
+      @amount = amount.to_f
     end
 
   end
--- spec/money_spec.rb
+++ spec/money_spec.rb
@@ -1,9 +1,23 @@
 RSpec.describe Exchange::Money do
 
+  before do
+    Exchange::Money.conversion_rates('EUR', {
+        'USD'     => 1.11,
+        'Bitcoin' => 0.0047
+    })
+  end
+
   describe '.new' do
     it 'returns an instance of Money' do
       expect(Exchange::Money.new(3, 'EUR')).to be_an_instance_of(Exchange::Money)
     end
   end
 
+  subject { Exchange::Money.new 50, 'EUR' }
+
+  describe '#amount' do
+    it 'returns amount (as Float)' do
+      expect(subject.amount).to be 50.0
+    end
+  end
 end
